f597c82b880c7242f3fa820d4f491caecc409df3
Malhar Dave <malharvdave@gmail.com>
2020-07-11 17:31:08 -0400

    ProxyCall.sol and tests added

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,9 @@
 const ProxyDelegate = artifacts.require('ProxyDelegate');
+const ProxyCall = artifacts.require('ProxyCall');
 const SomeLibrary = artifacts.require('SomeLibrary');
 
 module.exports = function(deployer) {
     deployer.deploy(SomeLibrary)
-    .then(() => deployer.deploy(ProxyDelegate, SomeLibrary.address));
+    .then(() => deployer.deploy(ProxyDelegate, SomeLibrary.address))
+    .then(() => deployer.deploy(ProxyCall, SomeLibrary.address));
 }
